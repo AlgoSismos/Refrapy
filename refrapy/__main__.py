75dfed04f5b14735777666d0fdad36a4432d9652
Marco Barbosa <aureliobarbosa@gmail.com>
2025-01-25 16:29:46 -0300

    add simple command line for calling refrapick and refrainv

--- refrapy/__main__.py
+++ refrapy/__main__.py
@@ -1,8 +1,34 @@
+import sys
 from refrapick import Refrapick
+from refrainv import Refrainv
 
 def main():
-    app = Refrapick()
-    app.mainloop()
+
+    default_args = ['pick', 'inv']
+
+    if len(sys.argv) == 1:      
+        print('Refrapy - Seismic Refraction Data Analysis\n'
+            'Refrapy needs an argument. \nCall "refrapy pick" or "refrapy inv"\n'
+            'for using the "pick" or "inv" apps.\n'
+            )
+        exit()
+    
+    arg = sys.argv[1]
+
+    if arg in default_args:
+        if arg == 'pick':
+            app = Refrapick()
+        elif arg == 'inv':
+            app = Refrainv()
+
+        app.mainloop()
+
+    else:
+        print('Refrapy - Seismic Refraction Data Analysis\n'
+            'Wrong usage. Call "refrapy pick" or "refrapy inv"\n'
+                'for using the "pick" or "inv" apps.\n'
+            )
+        exit()
 
 if __name__ == '__main__':
     main()
